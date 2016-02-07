b477f1a1bc43b9717a9016643685def31e436266
mrhankey2008 <mr.hankey2008@gmail.com>
2016-02-07 20:33:32 +0600

    first commit

---
+++ GameCore/Snake.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCore
+{
+    public class Snake
+    {
+        public Snake()
+        {
+            SetInitialSettings();
+        }
+
+        private void SetInitialSettings()
+        {
+            Length = 2;
+            Color = ConsoleColor.Green;
+            Speed = 2;
+        }
+        public int Length { get; private set; }
+
+        public ConsoleColor Color { get; set; }
+
+        public int Speed { get; set; }
+
+    }
+}
---
+++ Snake/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Snake.BL;
+namespace Snake
+{
+    class Program
+    {
+        private IGamePlay GamePlay;
+        static void Main(string[] args)
+        {
+        }
+    }
+}
